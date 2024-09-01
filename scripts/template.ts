9cd9dfbd0fe01b5662dc9be5d030c5772d00bbfa
roman910dev <roman910dev@gmail.com>
2024-09-01 22:12:52 +0200

    feat(template): :recycle: move templates to external gitignored file
    keep private data ignored

--- scripts/template.ts
+++ scripts/template.ts
@@ -1,46 +1,13 @@
 import { confirm } from '@inquirer/prompts'
 import { z } from 'zod'
 
+import type * as zs from '../src/utils/z-schemas'
 import db from '../src/db'
-import { defaultPayer } from '../src/db/config'
 import { transactions } from '../src/db/schema'
-import { typedObjectKeys, zodObjectInput } from '../src/utils'
+import { typedObjectKeys } from '../src/utils'
 import { transactionOpts } from '../src/utils/command-options'
-import * as zs from '../src/utils/z-schemas'
+import templates from '../src/utils/templates'
 import { zodCommand } from '../src/utils/zod-command'
-
-const templates = {
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	split: async ({ amount, from, to, ...tx }) => {
-		const { totalAmount, accounts, includeSelf } = await zodObjectInput({
-			totalAmount: zs.amount.describe('Total amount:'),
-			accounts: zs
-				.commasArray(zs.account)
-				.describe('Accounts to split between:'),
-			includeSelf: z.boolean().default(true).describe('Include self:'),
-		})
-		return [
-			{
-				amount: totalAmount,
-				from: from ?? defaultPayer,
-				to: 'NULL' as const,
-				...tx,
-			},
-			...accounts.map((account) => ({
-				amount: (
-					parseFloat(totalAmount) /
-					(accounts.length + (includeSelf ? 1 : 0))
-				).toFixed(2),
-				from: 'NULL' as const,
-				to: account,
-				...tx,
-			})),
-		]
-	},
-} satisfies Record<
-	string,
-	(tx: Partial<zs.InsertTx>) => Promise<Omit<zs.InsertTx, 'date'>[]>
->
 
 const getTxs = (
 	template: keyof typeof templates,
