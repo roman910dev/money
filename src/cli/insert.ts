20cc66cd0a3ed4b879ceff5f7f5b567c36c68445
Román Via-Dufresne Saus <roman910dev@gmail.com>
2026-06-01 18:06:47 +0200

    feat(input): :sparkles: add template support to `input` command

--- src/cli/insert.ts
+++ src/cli/insert.ts
@@ -1,10 +1,19 @@
+import z from 'zod'
 import { zodCommand } from 'zod-commander/zod4'
+import templates from '#/configs/templates.js'
 import db from '#/db/index.js'
 import { transactions } from '#/db/schema.js'
-import { zodObjectInput } from '#/utils/index.js'
+import { transactionStrOpts } from '#/utils/command-options.js'
+import { typedObjectKeys, zodObjectInput } from '#/utils/index.js'
 import * as zs from '#/utils/z-schemas.js'
-
 import { makeBalancesTable } from './balance.js'
+
+type TxKey = keyof zs.InsertTx
+type Template = {
+	default?: Partial<Record<TxKey, string>>
+	readonly?: Partial<Record<TxKey, string>>
+}
+export type Templates = Record<string, Template>
 
 export const afterInsert = async (
 	transactions: Pick<zs.Transaction, 'from' | 'to'>[],
@@ -17,8 +26,19 @@ export const afterInsert = async (
 const insert = zodCommand({
 	name: 'insert',
 	description: 'Insert a transaction into the money database',
-	async action() {
-		const transaction = await zodObjectInput(zs.transaction.shape)
+	opts: {
+		...transactionStrOpts,
+		template: z
+			.enum(typedObjectKeys(templates))
+			.optional()
+			.describe('t;The name of the template to use to prefill the transaction'),
+	},
+	async action(_, { template, ...tx }) {
+		const tp = template && templates[template]
+		const transaction = await zodObjectInput(zs.transaction.shape, {
+			default: tp?.default,
+			readonly: { ...tp?.readonly, ...tx },
+		})
 		await db.insert(transactions).values(transaction)
 		await afterInsert([transaction])
 	},
